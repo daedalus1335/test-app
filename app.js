421448303079496e1fa8f3a3ded67ad6a66482c7
daedalus1335 <unconfigured@null.spigotmc.org>
2017-01-27 14:49:08 -0800

    formatting

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var app = express();
 //MIDDLEWARE
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.posts('/', function(req, res){
-  res.render('posts-index', posts)
-})
+//app.posts('/', function(req, res){
+//  res.render('posts-index', posts)
+//})
 
 app.get('/', function (req, res) {
     res.render('home');
