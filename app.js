42155aa6e3f5a5fa2ec27a5d39e327365fb5c08f
daedalus1335 <unconfigured@null.spigotmc.org>
2017-01-31 21:39:15 -0800

    fixed post

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var Post = require('../models/post.js');
+var post = require('../models/post.js');
 
 var app = express();
 
