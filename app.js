932a63cbc8ab9275b06dd42274ce744ed2068deb
daedalus1335 <unconfigured@null.spigotmc.org>
2017-02-03 11:41:57 -0800

    Posts working/navbar addition

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var Post = require('../testapp/models/post.js');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/testapp');
 
 var app = express();
 
 //MIDDLEWARE
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(express.static('public'));
 
+//Routes
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/posts', function (req, res) {
-    Post.find(function(err, posts){
-        res.render('posts-index', posts);
-    })
-
-
+    var posts = []
+    res.render('posts-index', {posts: posts});
 });
 
 app.listen(process.env.PORT || 3000);
---
+++ public/scripts.js
@@ -0,0 +1,14 @@
+
+$( document ).ready(function() {
+    $( "#post-form" ).on("submit", function( event ) {
+    //   alert( "Handler for .submit() called." );
+      event.preventDefault();
+      var post = ( $(this).serialize());
+      console.log( post );
+      //console.log( $( this ).serialize() );
+      $('#posts').prepend("<div>" + $("#post-body").val() + "</div>");
+      $ajax({url: "posts-index"})
+    });
+
+    $( "#post-form" ).validate();
+});
